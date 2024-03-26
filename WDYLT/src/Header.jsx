6d8968797ab4f93ce8f73ce7bd3d6030edb8a112
Alejandro Matías Casco <alejandromatiascasco@gmail.com>
2024-03-26 15:45:25 -0300

    first commit

---
+++ WDYLT/src/Header.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h3>aprendiendo, un día a la vez.</h3>
+        <h1>¿Qué aprendsite hoy?</h1>
+      </header>
+    )
+  }
+}
---
+++ WDYLT/src/Navbar.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+        </ul>
+      </nav>
+    )
+  }
+}
