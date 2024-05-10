0e7ec7772e0558f5d3e5dbe2e9706cacb5a342b3
Alejandro Matías Casco <alejandromatiascasco@gmail.com>
2024-05-10 17:53:15 -0300

    slowly creating the navbar

--- WDYLT/src/Navbar.jsx
+++ WDYLT/src/Navbar.jsx
@@ -4,14 +4,26 @@ export default class Navbar extends Component {
   render() {
     return (
       <nav>
-        <ul>
-            <li><a href=""></a></li>
-            <li><a href=""></a></li>
-            <li><a href=""></a></li>
-            <li><a href=""></a></li>
-            <li><a href=""></a></li>
-            <li><a href=""></a></li>
-        </ul>
+        <div>
+          <div>
+            <ul>
+              <li><a href="">Inicio</a></li>
+              <li><a href="">Acerca de</a></li>
+              <li><a href="">Portfolio</a></li>
+              <li><a href="">Contacto</a></li>
+            </ul>
+            <form action="">
+              <input type="search" placeholder='Buscar...'/>
+            </form>
+            <div className='socials'>
+              <ul>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </nav>
     )
   }
